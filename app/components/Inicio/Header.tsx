4c6e4d26bdec24b128bfd0445e34ceb55556ec93
Daniel-L10N <alejandrolazolemus@gmail.com>
2026-04-22 13:26:23 -0600

    update ulr de header

--- app/components/Inicio/Header.tsx
+++ app/components/Inicio/Header.tsx
@@ -5,15 +5,15 @@ import Link from 'next/link';
 
 const navigation = {
   solutions: [
-    { name: 'Marketing ', href: '/servicios' },
-    { name: 'Analytics ', href: '/servicios' },
-    { name: 'Commerce ', href: '/servicios' },
-    { name: 'Insights', href: '/servicios' }
+    { name: 'Marketing ', href: '/servicios/marketing' },
+    { name: 'Analytics ', href: '/servicios/analytics' },
+    { name: 'Commerce ', href: '/servicios/commerce' },
+    { name: 'Insights', href: '/servicios/insights' }
   ],
   social: [
     {
       name: 'Facebook',
-      href: '#',
+      href: 'https://www.facebook.com/share/1EPnMYsVsd/',
       icon: (props: any) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -26,16 +26,16 @@ const navigation = {
     },
     {
       name: 'Twitter',
-      href: '#',
+      href: 'https://x.com/Control_Modular',
       icon: (props: any) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+          <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
         </svg>
       ),
     },
     {
       name: 'GitHub',
-      href: '#',
+      href: 'https://github.com/ControlModularMX/',
       icon: (props: any) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -46,6 +46,7 @@ const navigation = {
         </svg>
       ),
     },
+       
   ],
 };
 
@@ -93,7 +94,7 @@ export default function Header() {
             
             <div className="mt-12 flex gap-6">
               {navigation.social.map((item) => (
-                <a key={item.name} href={item.href} className="text-gray-400 hover:text-white transition duration-300">
+                <a key={item.name} target='_blank' href={item.href} className="text-gray-400 hover:text-white transition duration-300">
                   <span className="sr-only">{item.name}</span>
                   <item.icon className="h-8 w-8" aria-hidden="true" />
                 </a>
